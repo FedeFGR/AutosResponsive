f883c9523714b237eb67d10d98ecc8b8a2bd7117
FedeFGR <fedefgr@gmail.com>
2023-04-08 02:37:40 -0300

    actualizado

--- js/cargarAutos.js
+++ js/cargarAutos.js
@@ -1,10 +1,25 @@
+/*------------
 fetch("../BD/Autos.json")
 .then(response => {
    return response.json();
 })
 .then(data => console.log(data));
-
-/*const data = require('./employee.json');
-
-console.log(data);
 */
+
+const CatalogoContainer=document.getElementById("Catalogo");
+
+fetch("../BD/Autos.json")
+.then(response => {return response.json();})
+.then(data =>{
+   console.log(data)
+   for( const auto of data.Autos){
+      CatalogoContainer.innerHTML+=`
+      <div class="card">
+         <img class="imgcard" src="${auto.URL}">
+         <div class="cardescripcion">
+            <h4><b>${auto.Marca} ${auto.Modelo}</b></h4>
+            <p>${auto.Descripcion}</p>
+         </div>
+      </div>`
+   }
+})
